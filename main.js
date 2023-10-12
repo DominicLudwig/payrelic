6493b1da8a8b3045f156ce0056ffb46c8589d4b0
Dominic <53017484+DominicLudwig@users.noreply.github.com>
2023-10-12 13:18:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const typingSpeed = 50;
   let charIndex = 0;
   let toggle = false;
+  let iteration = 0
+  let maxIterations = 10;
 
   function waiting() {
+    if(iteration >= maxIterations) {
+      return;
+    }
     if(charIndex >= textToType.length){
       let text = typingContainer.textContent;
       if(!toggle) {
@@ -14,6 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
          text = text.slice(0, -1);
       }
       toggle = !toggle;
+      iteration++;
       typingContainer.textContent = text;
     }
     setTimeout(waiting, 500); 
