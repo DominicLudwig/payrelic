dbd745257035e39680b97d0c3b6271864fc33e4a
Dominic <53017484+DominicLudwig@users.noreply.github.com>
2023-10-12 11:55:28 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+  function typeText() {
+      if (charIndex < textToType.length) {
+          typingContainer.textContent += textToType.charAt(charIndex);
+          charIndex++;
+          setTimeout(typeText, typingSpeed);
+      }
+  }
+
+ document.addEventListener('DOMContentLoaded', function () {
+     const textToType = "-> Coming soon.";
+    const typingContainer = document.getElementById('typing-container');
+    const typingSpeed = 50;
+    let charIndex = 0;
+    typeText();
+ });
