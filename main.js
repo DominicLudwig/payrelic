81826ed1a373bea1472dd8d6d2d971f6e2538b30
Dominic <53017484+DominicLudwig@users.noreply.github.com>
2023-10-12 12:27:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
   let charIndex = 0;
   
   function typeText() {
-    if (charIndex < textToType.length) {
-      typingContainer.textContent += textToType.charAt(charIndex);
-      charIndex++;
+    if (charIndex <= textToType.length) {
+      let text = typingContainer.textContent;
+      if (text.length > 0) {
+        text = text.slice(0, -1);
+      }
+      if (charIndex < textToType.length) {
+        text += textToType.charAt(charIndex) + '_';
+      }
+      
+      typingContainer.textContent = text;
+      charIndex++; 
       setTimeout(typeText, typingSpeed);
-    }
+      }
   }
  
   typeText();
