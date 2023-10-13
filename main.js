f31f545e3f9400a6ad0bac87fc9bfc3ee483d961
ludwigapps <dominic.ludwig@gmx.net>
2023-10-13 09:36:06 +0200

    Reformatting

--- main.js
+++ main.js
@@ -1,56 +1,57 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const textToType = "-> Coming soon.";
-  const typingContainer = document.getElementById('typing-container');
-  const typingSpeed = 70;
-  const waitingSpeed = 500;
-  let charIndex = 0;
-  let toggle = false;
-  let iteration = 0
-  let maxIterations = 5;
+    const textToType = "-> Coming soon.";
+    const typingContainer = document.getElementById('typing-container');
+    const typingSpeed = 70;
+    const waitingSpeed = 500;
+    let charIndex = 0;
+    let toggle = false;
+    let iteration = 0
+    let maxIterations = 5;
 
-  function waiting() {
-    if(iteration >= maxIterations) {
-      return;
-    }
-    
-    let text = typingContainer.textContent;
-    if(!toggle && text.slice(-1) !== '_') {
-       text += '_';
-    }else if(text.slice(-1) === '_'){
-       text = text.slice(0, -1);
-    }
-    toggle = !toggle;
-    iteration++;
-    typingContainer.textContent = text;
-    
-    setTimeout(waiting, waitingSpeed); 
-  }
-  
-  function typeText() {
-    if (charIndex <= textToType.length) {
-      let text = typingContainer.textContent;
-      if (text.length > 0 && text.slice(-1) === '_') {
-        text = text.slice(0, -1);
-      }
-      if (charIndex < textToType.length) {
-        text += textToType.charAt(charIndex) + '_';
-      }
-      
-      typingContainer.textContent = text;
-      charIndex++; 
-      setTimeout(typeText, typingSpeed);
-      }
-  }
- 
-  waiting();
-  setTimeout(function(){
-     typeText();
-  }, maxIterations*waitingSpeed); 
+    function waiting() {
+        if (iteration >= maxIterations) {
+            return;
+        }
 
-  setTimeout(function(){
-    iteration = 0;
-    maxIterations = 20;
-    toggle = false;
+        let text = typingContainer.textContent;
+        if (!toggle && text.slice(-1) !== '_') {
+            text += '_';
+        } else if (text.slice(-1) === '_') {
+            text = text.slice(0, -1);
+        }
+        toggle = !toggle;
+        iteration++;
+        typingContainer.textContent = text;
+
+        setTimeout(waiting, waitingSpeed);
+    }
+
+    function typeText() {
+        if (charIndex <= textToType.length) {
+            let text = typingContainer.textContent;
+            if (text.length > 0 && text.slice(-1) === '_') {
+                text = text.slice(0, -1);
+            }
+            if (charIndex < textToType.length) {
+                text += textToType.charAt(charIndex) + '_';
+            }
+
+            typingContainer.textContent = text;
+            charIndex++;
+            setTimeout(typeText, typingSpeed);
+        }
+    }
+
     waiting();
-  }, (maxIterations*waitingSpeed)+(textToType.length*typingSpeed)+typingSpeed);
+
+    setTimeout(function () {
+        typeText();
+    }, maxIterations * waitingSpeed);
+
+    setTimeout(function () {
+        iteration = 0;
+        maxIterations = 20;
+        toggle = false;
+        waiting();
+    }, (maxIterations * waitingSpeed) + (textToType.length * typingSpeed) + typingSpeed);
 });
